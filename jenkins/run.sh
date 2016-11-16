49751627143268877cebacd263a38819bdb45b0f
Alexander Tarlinder <a@tarlinder.net>
2016-11-16 16:06:22 +0000

    Removed retrywget.sh since Jenkins supports similar functionality.

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 tarlinder/jenkins
+docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -e HOST=`hostname` tarlinder/jenkins
