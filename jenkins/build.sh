4eca0ad6a66fd4532c0fb527a56dc3dd12a8f0a0
Alexander Tarlinder <a@tarlidner.net>
2016-12-04 09:03:50 +0000

    Added html publisher plugin and replaced plugin fetching in base Jenkins.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -e
 WORKDIR=`dirname $0`
 
 if [ ! -f "$WORKDIR/jenkins.war" ]; then
-    wget -P $WORKDIR http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war
+    wget -q -P $WORKDIR http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war
+    ls -l $WORKDIR/jenkins.war
 else 
     echo "$WORKDIR/jenkins.war exists. Skipping download."
 fi
 
 if [ ! -d "$WORKDIR/plugins" ]; then
-    mkdir "$WORKDIR/plugins"
-    sh "$WORKDIR/get_plugins.sh"
+    $WORKDIR/get_plugins.pl
 else 
     echo "$WORKDIR/plugins exists. Omitting plugin download."
 fi
